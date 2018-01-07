0744c141ad2d67dd0b5226d87af2a52555014997
Masoud Ahadi <masoud.ahadi@gmail.com>
2018-01-07 15:05:39 +0330

    Prject setup.

---
+++ src/main/java/com/tehran/App.java
@@ -0,0 +1,13 @@
+package com.tehran;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
